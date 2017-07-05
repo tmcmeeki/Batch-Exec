984e189f73c07c897845b01019d18eb96c70a680
Tom McMeekin <tmcmeeki@cpan.org>
2017-07-05 15:13:20 +1000

    unit restructure

--- lib/Batch/Exec.pm
+++ lib/Batch/Exec.pm
@@ -24,6 +24,8 @@ Copyright (C) 2017  B<Tom McMeekin> E<lt>tmcmeeki@cpan.orgE<gt>
 The batch executive is a series of modules which provide a framework for
 batch processing.  The fundamental principles of the executive are as follows:
 
+=over 4
+
 =item a.  Log it.
 
 Provide as much detail as possible about what step you are up to and what
@@ -34,7 +36,7 @@ you're trying to do.
 Don't keep processing if the last command fails.  Always check that the
 last step succeeded.
 
-=item c.  Tidy it.
+=item c.  Tidy up.
 
 If you're creating a bunch of temporary files to assist with processing, then
 eventually you'll need to clean up after yourself.
@@ -63,8 +65,6 @@ basis for the other modules in this series.  Specifically, it covers
 shell execution and status checking, along with file/directory creation 
 and deletion.  It comprises the following attributes and methods:
 
-=over 4
-
 =cut
 
 #use 5.010000;
@@ -75,7 +75,9 @@ use warnings;
 use Carp qw(cluck confess);     # only use stack backtrace within class
 use Data::Dumper;
 use Cwd;
+use File::Basename;
 use File::Path;
+use Scalar::Util;	# check filehandle
 
 #use Batch::Log qw/ :all /;
 use Logfer qw/ :all /;
@@ -84,8 +86,7 @@ use vars qw/ @EXPORT $VERSION /;
 
 
 # --- package constants ---
-
-#use constant PN_TEMP => File::Spec->catfile($ENV{'HOME'}, "tmp");
+use constant PREFIX_TMP => basename($0);
 
 
 # --- package globals ---
@@ -96,15 +97,39 @@ our $AUTOLOAD;
 # --- package locals ---
 my $_n_objects = 0;     # counter of objects created.
 
+=over 4
+
+=item 1a.  OBJ->echo(EXPR)
+
+Echo the stdout from the B<execute()> method to the log.
+Takes a boolean (0 or 1) argument.
+Default is 0 (disabled).
+
+=item 1b.  OBJ->fatal(EXPR)
+
+Controls whether failed status checks "die".
+Default is 1 (enabled).
+
+=item 1c.  OBJ->prefix(EXPR)
+
+File prefix, defaults to the basename of the program (i.e. argv[0]).
+
+=item 1d.  OBJ->retry(EXPR)
+
+Retry count for the execute method.
+Default is 1 (attempt only once).
+
+=cut
+
 my %attribute = (
 	_n_objects => \$_n_objects,
 	_id => undef,
 	_log => get_logger("Batch::Exec"),
 	_dn_start => cwd,       # default this value, may need it later!
-	autoheader => 0,        # automatically put a header on tempfiles
-	echo => 0,              # echo stdout from execute
-	fatal => 1,             # controls whether failed checks "die"
-	retry => 1,		# default retry count
+	echo => 0,
+	fatal => 1,
+	prefix => PREFIX_TMP,
+	retry => 1,
 );
 
 
@@ -158,15 +183,11 @@ sub new {
 DESTROY {
 	my $self = shift;
 
-	$self->clean unless ($self->retain);
-
-	$self->purge unless ($self->retain);
-
 	-- ${ $self->{_n_objects} };
 }
 
 
-=item 1a.  OBJ->cough(EXPR)
+=item 2a.  OBJ->cough(EXPR)
 
 Issue warning or fatal message (EXPR) based on fatal attribute.
 
@@ -185,7 +206,7 @@ sub cough {
 }
 
 
-=item 1b.  OBJ->delete(EXPR)
+=item 2b.  OBJ->delete(EXPR)
 
 Remove the file or directory specified from the filesystem, i.e. unlink or
 this module's rmdir() function.
@@ -216,7 +237,7 @@ sub delete {	# delete a file or directory
 }
 
 
-=item 1c.  OBJ->execute([EXPR1], [ARRAY_REF], EXPR2, ...)
+=item 2c.  OBJ->execute([EXPR1], [ARRAY_REF], EXPR2, ...)
 
 Attempt to run the command (arguments starting with EXPR2) via readpipe().
 EXPR1 is the retry count (which defaults to 1).  Output can be stored in
@@ -237,11 +258,12 @@ sub execute {
 
 	$self->{'retry'} = 1 if (not defined $self->{'retry'});
 
-	$self->_log->debug("retry [$retry] command [$command]");
+	$self->_log->debug(sprintf "retry [%s] command [$command]",
+		$self->{'retry'});
 
-	for ($c_exec = 0; $c_exec < $self->{'retry; $c_exec++) {
+	for ($c_exec = 0; $c_exec < $self->{'retry'}; $c_exec++) {
 
-		$self->_log->debug("c_exec [$c_exec] retry [$retry]");
+		$self->_log->debug("c_exec [$c_exec]");
 
 		$stdout = readpipe($command);
 		$retval = $?;
@@ -253,153 +275,38 @@ sub execute {
 
 	for (split(/\n+/, $stdout)) {
 
-		$self->_log->info("stdout: $_")
-			if ($self->{'echo'});
+		if ($self->{'echo'}) {
+			$self->_log->info("stdout: $_");
+		}
 
 		push @$ra_stdout, $_
 			if (defined $ra_stdout);
 	}
 
-	return( $self->cough("command [$command] failed after $retry retries"))
+	return( $self->cough(sprintf "command [%s] failed after %d retries",
+		$command, $self->{'retry'}))
 		if ($retval ne 0);
 
 	return 0;
 }
 
 
-=item 2a.  OBJ->ckdir(EXPR)
+=item 2d.  OBJ->header(FILEHANDLE)
 
-Batch wrapper to the ckdir_rx method.
-
-=cut
-
-sub ckdir {
-	my $self = shift;
-	my $dn = shift;
-	confess "SYNTAX: ckdir(EXPR)" unless defined ($dn);
-
-	return 0 if ($self->ckdir_rx($dn));
-
-	return $self->cough("directory [$dn] does not exist");
-}
-
-=item 2b.  OBJ->ckdir_rx(EXPR)
-
-Check if EXPR is a valid and extant directory with read & execute privileges.
+Writes a nice header (generated-by program & timestamp) to the filehandle
+specified by FILEHANDLE.
+Will check if FILEHANDLE is both a filehandle and points to an open file.
 
 =cut
 
-sub ckdir_rx { 
+sub header {
 	my $self = shift;
-	my $dn = shift;
+	my $fh = openhandle(shift);
+	confess "SYNTAX: header(FILEHANDLE)" unless defined ($fh);
 
-	return 1
-		if (defined($dn) && $dn ne "" && -d $dn && -r $dn && -x $dn);
+	printf $fh "# ---- automatically generated by %s ----\n", $self->{'prefix'};
 
-	return 0;
-}
-
-=item 2c.  OBJ->ckdir_rwx(EXPR)
-
-Check if EXPR is writable in addition to result of ckdir_rx(EXPR) check.
-
-=cut
-
-sub ckdir_rwx { 
-	my $self = shift;
-	my $dn = shift;
-
-	return 1
-		if ($self->ckdir_rx($dn) && -w $dn);
-
-	return 0;
-}
-
-
-=item 2d.  OBJ->godir(EXPR)
-
-Attempt to navigate to the directory specified by EXPR.
-
-=cut
-
-sub godir {
-	my $self = shift;
-	my $dn = (@_) ? shift : $self->_dn_start;
-#	confess "SYNTAX: godir(EXPR)" unless defined ($dn);
-
-	$self->ckdir_rx($dn) || return($self->cough("invalid directory [$dn]"));
-
-	chdir($dn) || return($self->cough("chdir($dn) failed"));
-
-	$self->pwd;
-
-	return 0;
-}
-
-
-=item 2e.  OBJ->mkdir(EXPR)
-
-Creates the directory specified by EXPR.  Uses mkpath() to create
-multiple directories.
-
-=cut
-
-sub mkdir {
-	my $self = shift;
-	my $dn = shift;
-
-	return 0 if (-d $dn);
-
-	$self->_log->info("creating directory [$dn]");
-
-	mkpath($dn) || $self->cough("mkpath($dn) failed");
-
-	return ($self->cough("could not create directory [$dn]"))
-		unless (-d $dn);
-
-	return 0;
-}
-
-
-=item 2f.  OBJ->pwd(NULL)
-
-Echoes and returns current working directory, making use of cwd().
-
-=cut
-
-sub pwd {
-	my $self = shift;
-	
-	my $pwd = cwd;
-
-	$self->_log->info("now in directory [$pwd]");
-
-	return $pwd;
-}
-
-
-=item 2g.  OBJ->rmdir(EXPR)
-
-Removes the directory tree specified by EXPR.  Uses rmpath() to perform delete.
-
-=cut
-
-sub rmdir {
-	my $self = shift;
-	my $dn = shift;
-	confess "SYNTAX: rmdir(EXPR)" unless defined ($dn);
-
-	return( $self->cough("directory does not exist [$dn]"))
-		unless (-d $dn);
-
-	$self->_log->info("pruning directory [$dn]");
-
-	rmtree($dn) || $self->cough("rmtree($dn) failed");
-
-	return ($self->cough("could not prune directory [$dn]"))
-		if (-d $dn);
-
-	return 0;
+	printf $fh "# ---- timestamp %s ---- \n", scalar(localtime(time));
 }
 
 1;
@@ -432,7 +339,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 =head1 SEE ALSO
 
-L<perl>.
+L<perl>, L<Cwd>, L<File::Path>, L<Batch::Log>.
 
 =cut
 
